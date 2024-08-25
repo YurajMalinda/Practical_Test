73e368f97234dbf8a1a393ffb9ae6fc4715f030c
YurajMalinda <yurajmalinda123.com>
2024-08-25 09:19:14 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse66/springboot/practicaltest/PracticalTestApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.practicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse66/springboot/practicaltest/PracticalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springboot.practicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
