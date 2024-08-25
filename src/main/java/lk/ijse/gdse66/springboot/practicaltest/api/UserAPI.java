e97c4b69f00352e1800929952f83df5e99ef6130
YurajMalinda <yurajmalinda123.com>
2024-08-25 11:09:56 +0530

    Service layer modified

--- src/main/java/lk/ijse/gdse66/springboot/practicaltest/api/UserAPI.java
+++ src/main/java/lk/ijse/gdse66/springboot/practicaltest/api/UserAPI.java
@@ -1,41 +1,25 @@
 package lk.ijse.gdse66.springboot.practicaltest.api;
 
-import com.google.gson.Gson;
-import lk.ijse.gdse66.springboot.practicaltest.dto.UserDTO;
-import lk.ijse.gdse66.springboot.practicaltest.entity.User;
-import lombok.RequiredArgsConstructor;
-import okhttp3.*;
-import okhttp3.RequestBody;
+import lk.ijse.gdse66.springboot.practicaltest.service.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("api/v1/user")
-@RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class UserAPI {
-    private final OkHttpClient client = new OkHttpClient();
-    private final Gson gson = new Gson();
+    private final UserService userService;
+
+    public UserAPI(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/createUser")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> createUser(@org.springframework.web.bind.annotation.RequestBody UserDTO userDTO) {
-        String json = gson.toJson(userDTO);
-        RequestBody body = RequestBody.create(json, MediaType.get("application/json; charset=utf-8"));
-
-        Request request = new Request.Builder().url("https://5b380a1693a245608c1ff6f37433aa61.weavy.io").post(body).build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                return ResponseEntity.status(response.code()).body("Failed to create user");
-            }
-            return ResponseEntity.ok(response.body().string());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
-        }
+    public String createUser(@RequestBody String data) throws IOException {
+        return userService.create(data);
     }
 }
--- src/main/java/lk/ijse/gdse66/springboot/practicaltest/service/UserService.java
+++ src/main/java/lk/ijse/gdse66/springboot/practicaltest/service/UserService.java
@@ -1,4 +1,38 @@
 package lk.ijse.gdse66.springboot.practicaltest.service;
 
-public interface UserService {
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class UserService {
+    @Value("${weavy.api.url}")
+    private String apiUrl;
+
+    @Value("${weavy.api.key}")
+    private String apiKey;
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public String create(String jsonBody) throws IOException {
+        RequestBody body = RequestBody.create(jsonBody, MediaType.get("application/json"));
+
+        Request request = new Request.Builder()
+                .url(apiUrl + "/api/users")
+                .post(body)
+                .header("Authorization", "Bearer " + apiKey)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+            return response.body().string();
+        }
+    }
+
 }
